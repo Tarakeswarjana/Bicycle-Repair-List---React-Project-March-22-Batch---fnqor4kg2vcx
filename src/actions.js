ec3d7bdf960a999fb01247227f7627294c233782
Tarakeswarjana <tarakeswarjana0808@gmail.com>
2022-11-11 21:08:58 +0530

    new

--- src/actions.js
+++ src/actions.js
@@ -1,15 +1,52 @@
 /*
-Here all the actions are defined.
-Example of defining an actoin is as follows
 
-export const repairAdded = (var1,va2) => {
+
+*/export const repairAdded = ({ owner, model, description }) => {
   return {
-    type: "actionType1",
+    type: "repairAdded",
     payload: {
-      var1,
-      var2
-    }
-  }
-}
+      owner,
+      model,
+      description,
+    },
+  };
+};
 
-*/
+export const repairRemoved = (id) => {
+  return {
+    type: "repairRemoved",
+    payload: {
+      id,
+    },
+  };
+};
+export const repairResolved = (id) => {
+  return {
+    type: "repairResolved",
+    payload: {
+      id,
+    },
+  };
+};
+export const repairUpdate = ({ id, owner, model, description }) => {
+  return {
+    type: "repairUpdate",
+    payload: {
+      id,
+      owner,
+      model,
+      description,
+    },
+  };
+};
+export const editTask = ({ id, owner, model, description }) => {
+  return {
+    type: "editTask",
+    payload: {
+      id,
+      owner,
+      model,
+      description,
+    },
+  };
+};
